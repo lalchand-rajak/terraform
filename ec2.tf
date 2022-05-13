1fb0dfcc3e87f18f4592c9bee087b805d7e10655
Lalchandra Rajak <61547075+lalchand-rajak@users.noreply.github.com>
2022-05-13 11:37:26 +0530

    ec2.tf for github module

---
+++ ec2.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "xxxx"
+  secret_key = "xxx"
+}
+
+resource "aws_instance" "my-ec2" {
+  ami = "ami-04893cdb768d0f9ee"
+  instance_type = "t2.small"
+
+  tags = {
+    Name = "module-github"
+  }
+}
